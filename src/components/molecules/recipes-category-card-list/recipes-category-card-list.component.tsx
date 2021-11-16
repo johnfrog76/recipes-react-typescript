0d2094aa010a05bec303a856282ef28c582c5f59
John Webster <jwebster@valorem.com>
2021-11-16 14:05:18 -0800

    cleanup

--- src/components/molecules/recipes-category-card-list/recipes-category-card-list.component.tsx
+++ src/components/molecules/recipes-category-card-list/recipes-category-card-list.component.tsx
@@ -1,10 +1,7 @@
-import React, { useContext, useState, useEffect, FC } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { FC } from 'react';
 
 import { StyledCardContainer } from './recipes-category-card-list.styles';
-
 import RecipeCardItem from '../../atoms/recipe-card-item/recipe-card-item.component';
-import RecipeCollectionContext from '../../../contexts/recipe-collection/recipe-collection.context';
 import { iRecipe } from '../../../interfaces/recipe/recipe.interface';
 
 interface Props {
