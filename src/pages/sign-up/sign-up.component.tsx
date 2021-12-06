af143db9dbdf47a64b8dd37ac75d8d7d10b717af
John Webster <jwebster@Johns-MBP.hsd1.wa.comcast.net>
2021-12-06 15:58:49 -0800

    wip

--- src/pages/sign-up/sign-up.component.tsx
+++ src/pages/sign-up/sign-up.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { MainSection } from '../../components/atoms/main-section/main-section.component'
 import PageTitle from '../../components/atoms/page-title/page-title.component';
+
 const SignUpPage = () => (
     <MainSection>
         <PageTitle>Sign Up</PageTitle>
