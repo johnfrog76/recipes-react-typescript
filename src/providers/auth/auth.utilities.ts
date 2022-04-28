2bae42307dc05faf0f3161f79bf5b9415edfaa1b
John Webster <jwebster@valorem.com>
2022-04-28 13:27:28 -0700

    session expire logout

--- src/providers/auth/auth.utilities.ts
+++ src/providers/auth/auth.utilities.ts
@@ -17,6 +17,20 @@ export const setUserAuth = (user: iUser, expires: number):void => {
     )
 }
 
+export const getRemainingTime = () => {
+    const data:string | null = localStorage.getItem('userData');
+
+    if (data) {
+        const authData: StoredAuthType = JSON.parse(data);
+        const { expires } = authData;
+        const currentTime = new Date().getTime();
+        const remaining = expires - currentTime;
+        return remaining > 0 ? remaining : 0;
+    }
+
+    return 0;
+}
+
 export const expireAuth = () => {
     const data:string | null = localStorage.getItem('userData');
 
