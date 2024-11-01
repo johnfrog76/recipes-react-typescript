f3716da7d8212f10006504382abdac5eac41777b
John Webster <johnwebster@Johns-MacBook-Pro-2.local>
2024-11-01 16:03:55 -0700

    wip

--- src/services/favorites/favorites.service.ts
+++ src/services/favorites/favorites.service.ts
@@ -7,157 +7,132 @@ const addFavoriteBulkEndpoint = process.env.REACT_APP_BULK_FAVORITE_ADD;
 const removeFavoriteBulkEndpoint = process.env.REACT_APP_BULK_FAVORITE_REMOVE;
 
 export interface IFavoritesResponse {
-    message: string;
-    data: iRecipe;
+  message: string;
+  data: iRecipe;
 }
 
 export interface IFavoritesBulkResponse {
-    message: string;
-    data: iRecipe[];
-    errors: [];
+  message: string;
+  data: iRecipe[];
+  errors: [];
 }
 
 export interface IFavoritesService {
-    addFavorite: (
-        recipeId: string | undefined,
-        userId: string | undefined,
-        token: string | null
-      ) => Promise<IFavoritesResponse>;
+  addFavorite: (
+    recipeId: string | undefined,
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IFavoritesResponse>;
 
-    removeFavorite: (
-        recipeId: string | undefined,
-        userId: string | undefined,
-        token: string | null
-    ) => Promise<IFavoritesResponse>;
+  removeFavorite: (
+    recipeId: string | undefined,
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IFavoritesResponse>;
 
-    addFavoriteBulk: (
-        recipesList: string[],
-        userId: string | undefined,
-        token: string | null      
-    ) => Promise<IFavoritesBulkResponse>;
+  addFavoriteBulk: (
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IFavoritesBulkResponse>;
 
-    removeFavoriteBulk: (
-        recipesList: string[],
-        userId: string | undefined,
-        token: string | null      
-    ) => Promise<IFavoritesBulkResponse>;
+  removeFavoriteBulk: (
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IFavoritesBulkResponse>;
 }
-
-
 
 export class FavoritesService implements IFavoritesService {
-    addFavorite(
-        recipeId: string | undefined,
-        userId: string | undefined,
-        token: string | null
-    ) {
-        return new Promise<IFavoritesResponse>((resolve, reject) => {
-            axios({
-                method: "post",
-                url: `${baseURL}${addFavoriteEndpoint}`,
-                data: {
-                  recipeId: recipeId,
-                  userId: userId || "none",
-                },
-                headers: {
-                  Authorization: `Bearer ${token ? token : "none"}`,
-                },
-            })
-            .then((resp) => resolve(resp.data as IFavoritesResponse))
-            .catch((err) => reject(err));
-        });
-    }
+  addFavorite(
+    recipeId: string | undefined,
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IFavoritesResponse>((resolve, reject) => {
+      axios({
+        method: "post",
+        url: `${baseURL}${addFavoriteEndpoint}`,
+        data: {
+          recipeId: recipeId,
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => resolve(resp.data as IFavoritesResponse))
+        .catch((err) => reject(err));
+    });
+  }
 
-    removeFavorite(
-        recipeId: string | undefined,
-        userId: string | undefined,
-        token: string | null
-    ) {
-        return new Promise<IFavoritesResponse>((resolve, reject) => {
-            axios({
-                method: "post",
-                url: `${baseURL}${removeFavoriteEndpoint}`,
-                data: {
-                  recipeId: recipeId || "none",
-                  userId: userId || "none",
-                },
-                headers: {
-                  Authorization: `Bearer ${token ? token : "none"}`,
-                },
-              })
-            .then((resp) => resolve(resp.data as IFavoritesResponse))
-            .catch((err) => reject(err));
-        });
-    }
+  removeFavorite(
+    recipeId: string | undefined,
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IFavoritesResponse>((resolve, reject) => {
+      axios({
+        method: "post",
+        url: `${baseURL}${removeFavoriteEndpoint}`,
+        data: {
+          recipeId: recipeId || "none",
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => resolve(resp.data as IFavoritesResponse))
+        .catch((err) => reject(err));
+    });
+  }
 
-    addFavoriteBulk (
-        recipesList: string[],
-        userId: string | undefined,
-        token: string | null
-    ) {
-        return new Promise<IFavoritesBulkResponse>((resolve, reject) => {
-            axios({
-                method: "post",
-                url: `${baseURL}${addFavoriteBulkEndpoint}`,
-                data: {
-                  recipesList: recipesList,
-                  userId: userId || "none",
-                },
-                headers: {
-                  Authorization: `Bearer ${token ? token : "none"}`,
-                },
-            })
-            .then((resp) => {
-                resolve(resp.data as IFavoritesBulkResponse)
-            })
-            .catch((err) => reject(err));
-        });
-    }
+  addFavoriteBulk(
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IFavoritesBulkResponse>((resolve, reject) => {
+      axios({
+        method: "post",
+        url: `${baseURL}${addFavoriteBulkEndpoint}`,
+        data: {
+          recipesList: recipesList,
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => {
+          resolve(resp.data as IFavoritesBulkResponse);
+        })
+        .catch((err) => reject(err));
+    });
+  }
 
-    removeFavoriteBulk (
-        recipesList: string[],
-        userId: string | undefined,
-        token: string | null
-    ) {
-        return new Promise<IFavoritesBulkResponse>((resolve, reject) => {
-            axios({
-                method: "post",
-                url: `${baseURL}${removeFavoriteBulkEndpoint}`,
-                data: {
-                  recipesList: recipesList,
-                  userId: userId || "none",
-                },
-                headers: {
-                  Authorization: `Bearer ${token ? token : "none"}`,
-                },
-            })
-            .then((resp) => {
-                resolve(resp.data as IFavoritesBulkResponse)
-            })
-            .catch((err) => reject(err));
-        });
-    }
+  removeFavoriteBulk(
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IFavoritesBulkResponse>((resolve, reject) => {
+      axios({
+        method: "post",
+        url: `${baseURL}${removeFavoriteBulkEndpoint}`,
+        data: {
+          recipesList: recipesList,
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => {
+          resolve(resp.data as IFavoritesBulkResponse);
+        })
+        .catch((err) => reject(err));
+    });
+  }
 }
-
-//   export const removeFavoriteBulk = async (
-//     recipesList: string[],
-//     userId: string | undefined,
-//     token: string | null
-//   ) => {
-//     try {
-//       const resp = await axios({
-//         method: "post",
-//         url: `${baseURL}${removeFavoriteBulkEndpoint}`,
-//         data: {
-//           recipesList: recipesList,
-//           userId: userId || "none",
-//         },
-//         headers: {
-//           Authorization: `Bearer ${token ? token : "none"}`,
-//         },
-//       });
-//       return resp.data;
-//     } catch (err) {
-//       throw err;
-//     }
-//   };
--- src/services/recipes/recipes.services.ts
+++ src/services/recipes/recipes.services.ts
@@ -8,38 +8,61 @@ const baseURL = process.env.REACT_APP_BASEURL;
 const endpoint = process.env.REACT_APP_RECIPE_ENDPOINT;
 const commentEndpoint = process.env.REACT_APP_RECIPE_COMMENT_ENDPOINT;
 const copyRecipeEndpoint = process.env.REACT_APP_RECIPE_COPY;
-// const addFavoriteEndpoint = process.env.REACT_APP_FAVORITE_ADD;
-// const removeFavoriteEndpoint = process.env.REACT_APP_FAVORITE_REMOVE;
-// const addFavoriteBulkEndpoint = process.env.REACT_APP_BULK_FAVORITE_ADD;
-// const removeFavoriteBulkEndpoint = process.env.REACT_APP_BULK_FAVORITE_REMOVE;
 const addSharingBulkEndpoint = process.env.REACT_APP_BULK_SHARE_ADD;
 const removeSharingBulkEndpoint = process.env.REACT_APP_BULK_SHARE_REMOVE;
+
+export interface IShareResponse {
+  data: iRecipe[];
+  error: [];
+  message: string;
+}
 
 export interface IRecipeService {
   /** Unauth collection of recipes. */
   getRecipes: () => Promise<iRecipe[]>;
-  
+
   /** Auth collection of recipes. */
   getRecipesAuth: (token: string | null) => Promise<iRecipe[]>;
-  
+
   /** Copy a recipe from one user to another. */
   copyRecipe: (
     recipeId: string,
     userId: string,
     token: string | null
   ) => Promise<{ data: iRecipe; message: string }>;
-  
+
   /** Add a recipe. */
   addRecipe: (recipe: iRecipe, token: string | null) => Promise<iRecipe>;
-  
+
   /** Update a recipe. */
   updateRecipe: (recipe: iRecipe, token: string | null) => Promise<iRecipe>;
-  
+
   /** Delete a recipe. */
   removeRecipe: (
     recipe: iRecipe,
     token: string | null
   ) => Promise<{ message: string }>;
+
+  /** Comment on a recipe */
+  addRecipeComment: (
+    commentItem: iRecipeComment,
+    recipeId: string,
+    token: string | null
+  ) => Promise<iRecipe>;
+
+  /** Remove sharing from list of recipes */
+  removeShareBulk: (
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IShareResponse>;
+
+  /** Add sharing to list of recipes */
+  addShareBulk: (
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) => Promise<IShareResponse>;
 }
 
 export class RecipeService implements IRecipeService {
@@ -131,163 +154,67 @@ export class RecipeService implements IRecipeService {
         .catch((err) => reject(err));
     });
   }
+
+  addRecipeComment(
+    commentItem: iRecipeComment,
+    recipeId: string,
+    token: string | null
+  ) {
+    return new Promise<iRecipe>((resolve, reject) => {
+      axios({
+        method: "patch",
+        url: `${baseURL}${commentEndpoint}/${recipeId}`,
+        data: commentItem,
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => resolve(resp.data.recipe as iRecipe))
+        .catch((err) => reject(err));
+    });
+  }
+
+  removeShareBulk(
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IShareResponse>((resolve, reject) => {
+      axios({
+        method: "patch",
+        url: `${baseURL}${removeSharingBulkEndpoint}`,
+        data: {
+          recipesList: recipesList,
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => resolve(resp.data as IShareResponse))
+        .catch((err) => reject(err));
+    });
+  }
+
+  addShareBulk(
+    recipesList: string[],
+    userId: string | undefined,
+    token: string | null
+  ) {
+    return new Promise<IShareResponse>((resolve, reject) => {
+      axios({
+        method: "patch",
+        url: `${baseURL}${addSharingBulkEndpoint}`,
+        data: {
+          recipesList: recipesList,
+          userId: userId || "none",
+        },
+        headers: {
+          Authorization: `Bearer ${token ? token : "none"}`,
+        },
+      })
+        .then((resp) => resolve(resp.data as IShareResponse))
+        .catch((err) => reject(err));
+    });
+  }
 }
-
-// export const addFavorite = async (
-//   recipeId: string | undefined,
-//   userId: string | undefined,
-//   token: string | null
-// ) => {
-//   try {
-//     const resp = await axios({
-//       method: "post",
-//       url: `${baseURL}${addFavoriteEndpoint}`,
-//       data: {
-//         recipeId: recipeId,
-//         userId: userId || "none",
-//       },
-//       headers: {
-//         Authorization: `Bearer ${token ? token : "none"}`,
-//       },
-//     });
-//     return resp.data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-// export const removeFavorite = async (
-//   recipeId: string | undefined,
-//   userId: string | undefined,
-//   token: string | null
-// ) => {
-//   try {
-//     const resp = await axios({
-//       method: "post",
-//       url: `${baseURL}${removeFavoriteEndpoint}`,
-//       data: {
-//         recipeId: recipeId || "none",
-//         userId: userId || "none",
-//       },
-//       headers: {
-//         Authorization: `Bearer ${token ? token : "none"}`,
-//       },
-//     });
-//     return resp.data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-export const addRecipeComment = async (
-  commentItem: iRecipeComment,
-  recipeId: string,
-  token: string | null
-) => {
-  try {
-    const resp = await axios({
-      method: "patch",
-      url: `${baseURL}${commentEndpoint}/${recipeId}`,
-      data: commentItem,
-      headers: {
-        Authorization: `Bearer ${token ? token : "none"}`,
-      },
-    });
-
-    return resp.data.recipe;
-  } catch (err) {
-    throw err;
-  }
-};
-
-// export const addFavoriteBulk = async (
-//   recipesList: string[],
-//   userId: string | undefined,
-//   token: string | null
-// ) => {
-//   try {
-//     const resp = await axios({
-//       method: "post",
-//       url: `${baseURL}${addFavoriteBulkEndpoint}`,
-//       data: {
-//         recipesList: recipesList,
-//         userId: userId || "none",
-//       },
-//       headers: {
-//         Authorization: `Bearer ${token ? token : "none"}`,
-//       },
-//     });
-//     return resp.data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-// export const removeFavoriteBulk = async (
-//   recipesList: string[],
-//   userId: string | undefined,
-//   token: string | null
-// ) => {
-//   try {
-//     const resp = await axios({
-//       method: "post",
-//       url: `${baseURL}${removeFavoriteBulkEndpoint}`,
-//       data: {
-//         recipesList: recipesList,
-//         userId: userId || "none",
-//       },
-//       headers: {
-//         Authorization: `Bearer ${token ? token : "none"}`,
-//       },
-//     });
-//     return resp.data;
-//   } catch (err) {
-//     throw err;
-//   }
-// };
-
-export const removeShareBulk = async (
-  recipesList: string[],
-  userId: string | undefined,
-  token: string | null
-) => {
-  try {
-    const resp = await axios({
-      method: "patch",
-      url: `${baseURL}${removeSharingBulkEndpoint}`,
-      data: {
-        recipesList: recipesList,
-        userId: userId || "none",
-      },
-      headers: {
-        Authorization: `Bearer ${token ? token : "none"}`,
-      },
-    });
-    return resp.data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const addShareBulk = async (
-  recipesList: string[],
-  userId: string | undefined,
-  token: string | null
-) => {
-  try {
-    const resp = await axios({
-      method: "patch",
-      url: `${baseURL}${addSharingBulkEndpoint}`,
-      data: {
-        recipesList: recipesList,
-        userId: userId || "none",
-      },
-      headers: {
-        Authorization: `Bearer ${token ? token : "none"}`,
-      },
-    });
-    return resp.data;
-  } catch (err) {
-    throw err;
-  }
-};
